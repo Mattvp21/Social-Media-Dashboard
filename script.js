6b9b30d19cb6937b8f31f48ceb3d0258487900f6
Matthew Van Pelt <74370212+Mattvp21@users.noreply.github.com>
2021-07-02 14:16:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,24 @@ let statsNumberDisplay = document.querySelectorAll('.stats__details--number');
 checkBox.addEventListener('change', function() {
     if (checkBox.checked) {
         body.style.backgroundColor = "white";
+        body.style.transition = "1s ease-in";
         for(let i = 0; i < cardDisplay.length; i++) {
              cardDisplay[i].style.backgroundColor = 'hsl(227, 47%, 96%)';
+             cardDisplay[i].style.transform = 'rotateX(360deg)';
+             cardDisplay[i].style.transition = "1s ease-in";
              
         }
 
        
         for(let i = 0; i < headerDisplay.length; i++) {
             headerDisplay[i].style.color = 'black';
+            headerDisplay[i].style.transition = "1s ease-in";
+             
        }
 
        for(let i = 0; i < statsDisplay.length; i++) {
         statsDisplay[i].style.backgroundColor = 'hsl(227, 47%, 96%)';    
+        statsDisplay[i].style.transition = "1s ease-in";
    }
 
    for(let i = 0; i < cardNumberDisplay.length; i++) {
@@ -33,15 +39,22 @@ for(let i = 0; i < statsNumberDisplay.length; i++) {
        
     } else {
         body.style.backgroundColor = "";
+        body.style.transition = "1s ease-in";
         for(let i = 0; i < cardDisplay.length; i++) {
             cardDisplay[i].style.backgroundColor = 'hsl(228, 28%, 20%)';
+           cardDisplay[i].style.transform = 'rotateX(-360deg)';
+          
+            cardDisplay[i].style.transition = " 1s ease-in";
+            
        }
        for(let i = 0; i < headerDisplay.length; i++) {
         headerDisplay[i].style.color = 'white';
+        headerDisplay[i].style.transition = "1s ease-in";
+             
    }
    for(let i = 0; i < statsDisplay.length; i++) {
     statsDisplay[i].style.backgroundColor = 'hsl(228, 28%, 20%)';
-    
+    statsDisplay[i].style.transition = "1s ease-in";
 }
 
 for(let i = 0; i < cardNumberDisplay.length; i++) {
